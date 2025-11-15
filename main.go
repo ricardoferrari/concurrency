4e4c8c013deed25788d8b1001fdcad99b91e2f01
Ricardo A. Ferrari <ricardoaugusto.ferrari@gmail.com>
2025-11-15 18:52:26 -0300

    Replaces mutex with channel-based counter management
    Switches from mutex synchronization to a channel-based manager
    for concurrent counter updates, improving code clarity and
    eliminating explicit locking. Increases worker count and ensures
    safe aggregation of increments via message passing.

--- main.go
+++ main.go
@@ -5,38 +5,34 @@ import (
 	"sync"
 )
 
-func safeIncrement(counter *int, mu *sync.Mutex, wg *sync.WaitGroup) {
+func counterManager(sumRequest <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	mu.Lock()
-	*counter++
-	mu.Unlock()
+	var counter int = 0
+	for increment := range sumRequest {
+		counter += increment
+	}
+	fmt.Println("Final Counter from Manager:", counter)
 }
 
 func main() {
 
 	var wg sync.WaitGroup
+	var workersWg sync.WaitGroup
 
-	var counter int = 0
+	var sumRequest = make(chan int)
+	wg.Add(1)
+	go counterManager(sumRequest, &wg)
 
-	for range 1000 {
-		wg.Add(1)
+	for range 10000 {
+		workersWg.Add(1)
 		go func() {
-			defer wg.Done()
-			counter++
+			defer workersWg.Done()
+			sumRequest <- 1
 		}()
 	}
 
+	workersWg.Wait()
+
+	close(sumRequest)
 	wg.Wait()
-	fmt.Println("Final Counter:", counter)
-
-	var mu sync.Mutex
-	counter = 0
-
-	for range 1000 {
-		wg.Add(1)
-		go safeIncrement(&counter, &mu, &wg)
-	}
-
-	wg.Wait()
-	fmt.Println("Final Counter with Mutex:", counter)
 }
